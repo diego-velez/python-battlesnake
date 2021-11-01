78ba1473198193e74e4aeae4161132978f295504
DVT <DVT12@protonmail.com>
2021-11-01 12:01:54 -0400

    Added: - Function that calculates where snake should move to get to the first food that appeared food

--- server_logic.py
+++ server_logic.py
@@ -2,7 +2,7 @@ import random
 from typing import List, Dict
 
 
-def avoid_body(my_head: Dict[str, int], my_body: List[dict], possible_moves: List[str]) -> List[str]:
+def avoid_body(my_head: Dict[str, int], my_body: List[Dict[str, int]], possible_moves: List[str]) -> List[str]:
     for body_part in my_body[1:]:  # Exclude head from body
 
         # this body part is left of my head
@@ -48,6 +48,28 @@ def avoid_walls(my_head: Dict[str, int], board: Dict[str, int], possible_moves: 
     return possible_moves
 
 
+def travel_to_food(my_head: Dict[str, int], food: Dict[str, int], possible_moves: List[str]) -> str:
+    # Food is to the right of the head
+    if my_head["x"] < food["x"] and "right" in possible_moves:
+        return "right"
+
+    # Food is to the left of the head
+    elif my_head["x"] > food["x"] and "left" in possible_moves:
+        return "left"
+
+    # Food is above the head
+    elif my_head["y"] < food["y"] and "up" in possible_moves:
+        return "up"
+
+    # Food is below the head
+    elif my_head["y"] > food["y"] and "down" in possible_moves:
+        return "down"
+
+    # Allah-akbar
+    else:
+        return random.choice(possible_moves)
+
+
 def choose_move(data: dict) -> str:
     """
     data: Dictionary of all Game Board data as received from the Battlesnake Engine.
@@ -61,15 +83,17 @@ def choose_move(data: dict) -> str:
     my_head = data["you"]["head"]
     my_body = data["you"]["body"]
     board = data["board"]
+    food = board["food"][0]  # Make a b-line for the first meal that appears
     possible_moves = ["up", "down", "left", "right"]
 
     print(f"HEAD x:{my_head['x']}, y:{my_head['y']}")
+    print(f"FOOD x:{food['x']}, y:{food['y']}")
 
     possible_moves = avoid_body(my_head, my_body, possible_moves)
     possible_moves = avoid_walls(my_head, board, possible_moves)
 
-    move = random.choice(possible_moves)
-    # TODO: Explore new strategies for picking a move that are better than random
+    # Pick a move based on where the scooby snacks are
+    move = travel_to_food(my_head, food, possible_moves)
 
     print(f"Turn #{data['turn']}: {move} picked from all valid options in {possible_moves}\n")
 
